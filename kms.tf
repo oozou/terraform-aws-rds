d4361737bf9f77b94ce6ab54e9cf97e57b93a366
sedthawut tipkanpirome <sedthawut2000@gmail.com>
2022-05-09 18:27:12 +0700

    (update): kms version from v0.0.1 to v0.0.2; support policy

--- kms.tf
+++ kms.tf
@@ -4,10 +4,21 @@
 module "rds_kms" {
   count = var.is_create_db_instance && var.storage_encrypted ? 1 : 0
 
-  source      = "git@github.com:oozou/terraform-aws-kms-key.git?ref=v0.0.1"
-  key_type    = "service"
-  description = "Used to encrypt data in ${local.identifier}"
-  alias_name  = "${local.identifier}-kms"
+  source = "git@github.com:oozou/terraform-aws-kms-key.git?ref=v0.0.2"
 
-  custom_tags = merge(local.tags, { "Name" : "${local.identifier}-kms" })
+  prefix               = var.prefix
+  name                 = "${local.identifier}-kms"
+  environment          = var.environment
+  key_type             = "service"
+  description          = "Used to encrypt data in ${local.identifier}"
+  append_random_suffix = true
+
+  service_key_info = {
+    caller_account_ids = [data.aws_caller_identity.main.account_id]
+    aws_service_names  = ["rds.${data.aws_region.active.name}.amazonaws.com"]
+  }
+
+  additional_policies = var.additional_kms_key_policies
+
+  tags = merge(local.tags, { "Name" : "${local.identifier}-kms" })
 }
