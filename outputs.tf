4933bed1f4245236257360594ee846c1eda4204a
Brian - oozou <98243528+lycbrian@users.noreply.github.com>
2025-04-24 10:55:51 +0700

    add secret arn output

--- outputs.tf
+++ outputs.tf
@@ -118,3 +118,7 @@ output "db_client_security_group_id" {
 output "secret_manager_postgres_creds_key" {
   value = aws_secretsmanager_secret.postgres_creds[0].name
 }
+
+output "secret_manager_postgres_creds_arn" {
+  value = aws_secretsmanager_secret.postgres_creds[0].arn
+}
